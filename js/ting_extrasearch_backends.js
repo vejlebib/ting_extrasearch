80c8eb9b124f83b2482f10bcf502e521978c608c
wiredloose <ea@vejlebib.dk>
2014-02-15 12:51:02 +0100

    adding javascript to move search input into content area when on the search result page

--- js/ting_extrasearch_backends.js
+++ js/ting_extrasearch_backends.js
@@ -24,5 +24,35 @@
         return true;
       }
     });
+
+    // Moves the search form into the search result page.
+    if (window.location.href.indexOf("search/tingextra") > -1) {
+      var search = $('.search-field-wrapper');
+      search.addClass('search-field-in-content');
+      search.addClass('js-search-field-in-content');
+
+      // Remove label.
+      search.find('label').remove();
+
+      // Add the search field.
+      var element = $('.pane-extrasearch-result-count');
+      if (element.length) {
+        search.insertAfter('.pane-extrasearch-result-count');
+      }
+      else {
+        search.insertBefore('.view-node-extrasearch-result .search-results');
+      }
+
+      // Ensure that the spinner and other stuff works by wrapping it in a
+      // search div.
+      search.wrap('<div class="search"></div>');
+
+      // Remove tabs (panels visibility rules do not work!).
+      $('.pane-page-tabs').remove();
+
+      // Hide top menu bar link.
+      $('.topbar-link-search').hide();
+    }
+
   });
 }(jQuery));
